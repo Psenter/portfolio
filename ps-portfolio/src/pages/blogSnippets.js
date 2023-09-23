a428bd934f73e4aabcf9d3840eb1003f46ffce14
Parker S <111610335+Psenter@users.noreply.github.com>
2023-09-22 20:31:46 -0400

    Made blog snippets page.

---
+++ ps-portfolio/src/pages/blogSnippets.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Image from "next/image";
+import "./aboutMe.css";
+
+export default function BlogSnippets() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
